00f38cd186e2c7d4242f3f859d24741e700892a8
TheMuffinMan2 <54967247+TheMuffinMan2@users.noreply.github.com>
2019-11-23 19:42:55 -0800

    Update myparser.cpp

--- myparser.cpp
+++ myparser.cpp
@@ -267,12 +267,18 @@ int main()
 // ** Need syntaxerror1 and syntaxerror2 functions (each takes 2 args)
 //    to display syntax error messages as specified by me.  
 
-// Type of error: **
-// Done by: ** 
-void syntaxerror1(  ){    }
-// Type of error: **
-// Done by: ** 
-void syntaxerror2(  ) {    }
+// Type of error: match failed
+// Done by: Justin Flores 
+void syntaxerror1(string s, tokentype t)
+{    
+  cout << "SYNTAX ERROR: expected " << t << " but found " << s << endl;
+}
+// Type of error: switch default
+// Done by: Justin Flores 
+void syntaxerror2(string s, string function) 
+{    
+  cout << "SYNTAX ERROR: unexpected " << s << " found in " << function << endl;
+}
 
 // ** Need the updated match and next_token with 2 global vars
 // saved_token and saved_lexeme
@@ -376,6 +382,89 @@ void after_noun() {
   default:
     syntaxerror2(saved_lexeme, "<after noun>");
   }
+}
+
+// Grammar: <after object>::=<verb><tense>PERIOD|<noun>DESTINATION<verb><tense>
+//PERIOD
+// Done by: Justin Flores
+void after_object()
+{
+  switch(next_token()){
+  case WORD2:
+    verb();
+    tense();
+    match(PERIOD);
+    break;
+  case (WORD1 || PRONOUN):
+    noun();
+    match(DESTINATION);
+    verb();
+    tense();
+    match(PERIOD);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "<after object>");
+  }
+}
+// Grammar: <noun>::= WORD1|PRONOUN
+// Done by: Justin Flores
+void noun()
+{
+  cout << "Processing <noun>\n";
+  switch(next_token()){
+  case WORD1:
+    match(WORD1);
+    break;
+  case PRONOUN:
+    match(PRONOUN);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "<noun>");
+  }
+}
+// Grammar: <verb>:: =WORD2
+// Done by: Justin Flores
+void verb()
+{
+  cout << "Processing <verb>\n";
+  match(WORD2);
+}
+// Grammar: <be>::= IS|WAS
+// Done by: Justin Flores
+void be()
+{
+  cout << "Processing <be>\n";
+  switch(next_token()){
+  case IS:
+    match(IS);
+    break;
+  case WAS:
+    match(WAS);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "<be>");
+  }
+}
+// Grammar: <tense>::=VERBPAST|VERBPASTNEG|VERB|VERBNEG
+// Done by: Justin Flores
+void tense()
+{
+  cout << "Processing <tense>\n";
+  switch(next_token()){
+  case VERBPAST:
+    match(VERBPAST);
+    break;
+  case VERBPASTNEG:
+    match(VERBPASTNEG);
+    break;
+  case VERB:
+    match(VERB);
+    break;
+  case VERBNEG:
+    match(VERBNEG);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "<tense>");
 }
 //----------- Driver ---------------------------
 
